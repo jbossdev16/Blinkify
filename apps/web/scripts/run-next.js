17882ddd7b69aa6c1b39712be3a416fa5e782849
jbossdev16 <jboss.dev16@gmail.com>
2026-04-07 20:21:42 +0200

    dwadwa

--- apps/web/scripts/run-next.js
+++ apps/web/scripts/run-next.js
@@ -1,6 +1,12 @@
 #!/usr/bin/env node
+const path = require("path");
 const { execFileSync } = require("child_process");
-const bin = require.resolve("next/dist/bin/next");
+
+const root = path.resolve(__dirname, "..", "..", "..");
+const bin = require.resolve("next/dist/bin/next", {
+  paths: [path.join(root, "node_modules"), __dirname],
+});
+
 try {
   execFileSync(process.execPath, [bin, ...process.argv.slice(2)], {
     stdio: "inherit",
